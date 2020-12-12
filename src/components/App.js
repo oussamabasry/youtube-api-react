b2f2fe7c27c2e46e9a030966d65999272ba71825
oussamabasry <obasry@gmail.com>
2020-12-12 16:36:01 +0100

    iframe

---
+++ src/components/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import youtube from '../apis/youtube';
+import SearchBar from './SearchBar';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
+
+
+
+
+class App extends React.Component {
+
+    state = {
+        videos: [],
+        selectedVideo: null
+    };
+
+    OnTermSubmit = async (term) => {
+        const response = await youtube.get('/search', {
+            params: {
+                q: term
+            }
+        });
+        this.setState({ videos: response.data.items });
+        this.setState({ selectedVideo: null })
+    };
+
+    onVideoSelect = (video) => {
+        this.setState({ selectedVideo: video });
+    };
+
+    render() {
+        return (
+            <div className="ui container">
+                <SearchBar onFormSubmit={this.OnTermSubmit} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
+            </div>
+
+        );
+    }
+};
+
+
+export default App;
---
+++ src/components/VideoItem.js
@@ -0,0 +1,21 @@
+import './VideoItem.css';
+import React from 'react';
+
+
+
+
+const VideoItem = ({ video, onVideoSelect }) => {
+ 
+  
+    return (
+        <div className="video-item ui item" onClick={ () => onVideoSelect(video)}>
+            <img className="ui image" src={video.snippet.thumbnails.medium.url} alt="" />
+            <div className="content">
+                <div className="header">{video.snippet.title}</div>
+            </div>
+
+        </div>
+    );
+};
+
+export default VideoItem;
